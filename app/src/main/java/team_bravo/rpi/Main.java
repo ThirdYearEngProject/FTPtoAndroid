6f87d801bc8e67a326d35b381413cc86626feafb
checkandmate <mate_marinkovic@hotmail.com>
2016-03-02 15:06:00 -0500

    First

---
+++ app/src/main/java/team_bravo/rpi/Main.java
@@ -0,0 +1,13 @@
+package team_bravo.rpi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
