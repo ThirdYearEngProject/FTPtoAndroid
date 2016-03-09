948b0f337e92c9369cb2bd60929ff5687d6f3bd3
dusanboskovic <dusanbosk@gmail.com>
2016-03-09 11:08:36 -0500

    Dusan - TEST FROM MARCH 08

--- app/src/main/java/team_bravo/rpi/Main.java
+++ app/src/main/java/team_bravo/rpi/Main.java
@@ -1,26 +1,42 @@
 package team_bravo.rpi;
 
+import android.content.pm.PackageInstaller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jcraft.jsch.*;
+
+import java.util.ArrayList;
+
 public class Main extends AppCompatActivity {
+
+    PackageInstaller.Session session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
 
-public class Password {
+    public void ConnectSSH() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSch jsch = new JSch();
+                    Profile p = Profiles.get(CurrProfile);
+                    session = jsch.getSession(p.Username, p.IpAddress, 22);
+                    session.setPassword(p.Password);
+                    Properties config = new Properties();
+                    config.put("StrictHostKeyChecking", "no");
+                    session.setConfig(config);
+                    session.connect();
 
-    public DetermineState();
-
-    public int ReturnCurrentValue(){
-
-        return 0;
+                    StartUpdateLoop();
+                } catch (final Exception e) {
+                    ThrowException(e.getMessage());
+                }
+            }
+        }).start();
     }
-
-
-
 }
